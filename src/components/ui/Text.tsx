c2c6711e8846626afaedc4714088987027d3cdac
Arham utt011 <arhamhamidbutt@gmail.com>
2025-04-09 10:18:48 +0500

    first commit

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,21 +1,27 @@
-import React, { forwardRef } from "react";
-import { cn } from "@/utils";
+import React, { forwardRef } from 'react'
+import { cn } from '@/utils'
 
-type ComponentAs = "h1" | "h2" | "p";
+type ComponentAs = 'h1' | 'h2' | 'p'
 
 interface Props {
-  children?: React.ReactNode;
-  className?: string;
-  as?: ComponentAs;
-  onclick?: React.MouseEventHandler;
-  Style?: React.CSSProperties;
-  dangerouslySetInnerHTML?: { __html: string }; // Add this prop
+  children?: React.ReactNode
+  className?: string
+  as?: ComponentAs
+  onclick?: React.MouseEventHandler
+  Style?: React.CSSProperties
+  dangerouslySetInnerHTML?: { __html: string } // Add this prop
 }
 
 const Text = forwardRef<HTMLHeadingElement | HTMLParagraphElement, Props>(
   (props, ref) => {
-    const { children, className, as, onclick, Style, dangerouslySetInnerHTML } =
-      props;
+    const {
+      children,
+      className,
+      as,
+      onclick,
+      Style,
+      dangerouslySetInnerHTML,
+    } = props
 
     if (dangerouslySetInnerHTML) {
       // If dangerouslySetInnerHTML is passed, render using this
@@ -23,62 +29,62 @@ const Text = forwardRef<HTMLHeadingElement | HTMLParagraphElement, Props>(
         <p
           ref={ref}
           className={cn(
-            "font-raleway text-primary text-[23px] font-normal leading-[33.2px]",
-            className
+            'font-raleway text-primary text-[23px] font-normal leading-[33.2px]',
+            className,
           )}
           onClick={onclick}
           style={Style}
           dangerouslySetInnerHTML={dangerouslySetInnerHTML} // Use this here
         />
-      );
+      )
     }
 
-    if (as === "h1") {
+    if (as === 'h1') {
       return (
         <h1
           ref={ref}
           className={cn(
-            "font-raleway text-[#FFFFFF] text-[44.91px] mob:text-[40px] font-bold leading-[60px] mob:leading-[45px]",
-            className
+            'font-raleway text-[#FFFFFF] sm:text-[44.91px] text-[40px] font-bold sm:leading-[60px] leading-[45px]',
+            className,
           )}
           onClick={onclick}
           style={Style}
         >
           {children}
         </h1>
-      );
+      )
     }
 
-    if (as === "h2") {
+    if (as === 'h2') {
       return (
         <h2
           ref={ref}
           className={cn(
-           "font-raleway text-[#FFFFFF] text-[27px]  font-semibold leading-[28px] ",
-            className
+            'font-raleway text-[#FFFFFF] text-[27px]  font-semibold leading-[28px] ',
+            className,
           )}
           onClick={onclick}
         >
           {children}
         </h2>
-      );
+      )
     }
 
     return (
       <p
         ref={ref}
         className={cn(
-         "font-raleway text-primary text-[23px] font-normal leading-[33.2px]",
-          className
+          'font-raleway text-primary text-[23px] font-normal leading-[33.2px]',
+          className,
         )}
         onClick={onclick}
       >
         {children}
       </p>
-    );
-  }
-);
+    )
+  },
+)
 
-Text.displayName = "Text";
+Text.displayName = 'Text'
 
-export default Text;
+export default Text
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        raleway: ["var(--font-raleway)"],
+        raleway: ['var(--font-raleway)'],
       },
       colors: {
-        primary: "#FFFFFF99",
-        accent: "#A8E543",
+        primary: '#FFFFFF99',
+        accent: '#A8E543',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
 
-      screens: {
-        "2xl": { max: "1535px" },
-        xl: { max: "1279px" },                      
-        lg: { max: "1023px" },
-        tab: { max: "991px" },
-        mob: { max: "768px" },
-      },
+      // screens: {
+      //   '2xl': { max: '1535px' },
+      //   xl: { max: '1279px' },
+      //   lg: { max: '1023px' },
+      //   tab: { max: '991px' },
+      //   mob: { max: '768px' },
+      // },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
